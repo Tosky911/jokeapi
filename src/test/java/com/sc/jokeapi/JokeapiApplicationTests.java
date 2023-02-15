c6d2a29cbca58910d75885adb38b942588e830e1
Santiago Carmona <santiagocarmona911@gmail.com>
2023-02-15 00:46:39 -0500

    Initial setup commit

---
+++ src/test/java/com/sc/jokeapi/JokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.jokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
