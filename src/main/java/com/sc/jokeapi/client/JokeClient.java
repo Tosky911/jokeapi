d830cb4bcc99d25b915558dcc027386c95b28472
Santiago Carmona <santiagocarmona911@gmail.com>
2023-02-15 23:24:24 -0500

    create config class and webclient bean

--- src/main/java/com/sc/jokeapi/client/JokeClient.java
+++ src/main/java/com/sc/jokeapi/client/JokeClient.java
@@ -1,6 +1,7 @@
 package com.sc.jokeapi.client;
 
 import com.sc.jokeapi.model.JokeOTDResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,14 +10,10 @@ import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@RequiredArgsConstructor
 public class JokeClient {
 
-    //Builder pattern style fluent-API
-    WebClient webClient = WebClient.builder()
-            .baseUrl("https://api.jokes.one")
-            .defaultCookie("cookie-name", "cookie-value")
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .build();
+    private final WebClient webClient;
 
     public JokeOTDResponse getJokeOfTheDay(){
         return webClient.get()
